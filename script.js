7f5f35f372264f0241ddccfda4af6b8ff8210f1d
andreaiaia <bianan96@gmail.com>
2021-01-10 21:13:41 +0100

    moved files

---
+++ script.js
@@ -0,0 +1,40 @@
+window.onscroll = function() {
+  if (document.documentElement.clientWidth < 800) {
+    scrollFunction();
+  }
+};
+
+function scrollFunction() {
+  const title = document.querySelector("#title");
+  const head = document.querySelector("HEADER");
+  const navp = document.querySelectorAll(".navp");
+
+  if (document.body.scrollTop > 35 || document.documentElement.scrollTop > 35) {
+    head.style.height = "6.5rem";
+
+    title.style.fontSize = "1.7rem";
+    title.style.marginTop = "1.1rem";
+    title.style.marginBottom = ".6rem";
+
+    navp.forEach(element => {
+      element.style.opacity = "0";
+      // element.style.display = "none";      
+    });
+    navp.forEach(element => {
+      element.style.display = "none";      
+    });
+  } else {
+    head.style.height = "9.5rem";
+
+    title.style.fontSize = "2.1rem";
+    title.style.marginTop = "1.8rem";
+    title.style.marginBottom = "1rem";
+
+    navp.forEach(element => {
+      element.style.opacity = "1";
+    });
+    navp.forEach(element => {
+      element.style.display = "block";      
+    });
+  }
+}
