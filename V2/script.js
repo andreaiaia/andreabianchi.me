ebc5bb41a84522646f571c96229d97509c5c7c0e
Andrea Bianchi <bianan96@gmail.com>
2021-01-10 15:40:42 +0100

    navbar improved

--- V2/script.js
+++ V2/script.js
@@ -3,18 +3,27 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   const title = document.querySelector("#title");
   const head = document.querySelector("HEADER");
+  const navp = document.querySelectorAll(".navp");
 
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    head.style.height = "16%";
+    head.style.height = "13%";
 
     title.style.fontSize = "1.7rem";
     title.style.marginTop = "1.1rem";
     title.style.marginBottom = ".6rem";
+
+    navp.forEach(element => {
+      element.style.display = "none";      
+    });
   } else {
     head.style.height = "20%";
 
     title.style.fontSize = "2.1rem";
     title.style.marginTop = "1.8rem";
     title.style.marginBottom = "1rem";
+
+    navp.forEach(element => {
+      element.style.display = "block";      
+    });  
   }
 }
