186fe7c81666586d73491c68c7a5b3a130682778
andreaiaia <andreaiaia@hotmail.it>
2021-01-24 22:46:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -50,6 +50,49 @@ function init_footer() {
 
 init_footer();
 
+// NAV SHRINK ANIMATION
+
+window.onscroll = function() {
+  if (document.documentElement.clientWidth < 800) {
+    scrollFunction();
+  }
+};
+
+function scrollFunction() {
+  const title = document.querySelector("#title");
+  const head = document.querySelector("HEADER");
+  const navp = document.querySelectorAll(".navp");
+
+  if (document.body.scrollTop > 35 || document.documentElement.scrollTop > 35) {
+    head.style.height = "6rem";
+
+    title.style.fontSize = "1.7rem";
+    title.style.marginTop = "1.1rem";
+    title.style.marginBottom = ".6rem";
+
+    navp.forEach(element => {
+      element.style.opacity = "0";
+      // element.style.display = "none";      
+    });
+    navp.forEach(element => {
+      element.style.display = "none";      
+    });
+  } else {
+    head.style.height = "9rem";
+
+    title.style.fontSize = "2.1rem";
+    title.style.marginTop = "1.8rem";
+    title.style.marginBottom = "1rem";
+
+    navp.forEach(element => {
+      element.style.opacity = "1";
+    });
+    navp.forEach(element => {
+      element.style.display = "block";      
+    });
+  }
+}
+
 // INTERACTIVE BIO HANDLERS
 
 const wrap = document.querySelector("#range_wrap");
@@ -102,48 +145,5 @@ function setBio(range) {
             <p>Anyway the slider says this is the funny bio, so here is a <em>joke</em>:</p>
             <p>A doctor, an architect and a programmer talk about their professions. "Mine is the oldest", says the doctor, "as everybody knows God created Eve from the rib of Adam, and that's definitely a medical operation". "Right", says the architect, "but in fact architect is even older - it's definitely an architectural project to create the world from chaos". At this point the programmer kicks back in the chair and gives friends a mysterious look. "Who, do you think, <em>created the chaos?</em>".</p>
             `;
-  }
-}
-
-// NAV SHRINK ANIMATION
-
-window.onscroll = function() {
-  if (document.documentElement.clientWidth < 800) {
-    scrollFunction();
-  }
-};
-
-function scrollFunction() {
-  const title = document.querySelector("#title");
-  const head = document.querySelector("HEADER");
-  const navp = document.querySelectorAll(".navp");
-
-  if (document.body.scrollTop > 35 || document.documentElement.scrollTop > 35) {
-    head.style.height = "6rem";
-
-    title.style.fontSize = "1.7rem";
-    title.style.marginTop = "1.1rem";
-    title.style.marginBottom = ".6rem";
-
-    navp.forEach(element => {
-      element.style.opacity = "0";
-      // element.style.display = "none";      
-    });
-    navp.forEach(element => {
-      element.style.display = "none";      
-    });
-  } else {
-    head.style.height = "9rem";
-
-    title.style.fontSize = "2.1rem";
-    title.style.marginTop = "1.8rem";
-    title.style.marginBottom = "1rem";
-
-    navp.forEach(element => {
-      element.style.opacity = "1";
-    });
-    navp.forEach(element => {
-      element.style.display = "block";      
-    });
   }
 }
