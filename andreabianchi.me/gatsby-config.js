190255d48003fe75f69cf26c4f52c7bf2c3f1514
Andrea Bianchi <bianan96@gmail.com>
2022-02-20 21:50:03 +0100

    modified:   gatsby-config.js

--- andreabianchi.me/gatsby-config.js
+++ andreabianchi.me/gatsby-config.js
@@ -6,6 +6,15 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    'gatsby-plugin-catch-links',
+    {
+	resolve: 'gatsby-source-filesystem',
+	options: {
+	    path: `${__dirname}/src/pages`,
+	    name: 'pages',
+	},
+    },
+    'gatsby-transformer-remark',
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
